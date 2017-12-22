75401d6d0f1eea12a50d75128b6f24c820ef8263
piotrgryta <piotrek.gryta@gmail.com>
2017-12-22 19:55:25 +0100

    refactor -> UserServiceImpl

--- bookstore/src/main/java/pl/agh/bookstore/services/UserServiceImpl.java
+++ bookstore/src/main/java/pl/agh/bookstore/services/UserServiceImpl.java
@@ -3,44 +3,37 @@ package pl.agh.bookstore.services;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import pl.agh.bookstore.connection.ConnectionPool;
 import pl.agh.bookstore.connection.DButil;
+import pl.agh.bookstore.model.dbtables.UsersTable;
 import pl.agh.bookstore.security.model.LoginResults;
 import pl.agh.bookstore.security.model.User;
 import pl.agh.bookstore.security.model.UserTypes;
 
 public class UserServiceImpl implements UserService{
 
-	//@Autowired
-	//ConnectionPool connectionPool;
-	
 	@Override
 	public LoginResults logUser(String username, String password) throws Exception {
-
-		String query = "Select * from users.users where username = ? and active = true";
+		StringBuilder query = new StringBuilder("Select * from users.users where ");
+		query.append(UsersTable.USERNAME.getColumnName() + " = ? and ");
+		query.append(UsersTable.ACTIVE.getColumnName() + " = true");
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-			con = DButil.getConnection();//connectionPool.getConnectionPool();
-			ps = con.prepareStatement(query);
+			con = DButil.getConnection();
+			ps = con.prepareStatement(query.toString());
 			ps.setString(1, username);
-
 			rs = ps.executeQuery();
 			User user = null;
 			while (rs.next()) {
 				user = new User();
-				user.setPassword(rs.getString("password"));
+				user.setPassword(rs.getString(UsersTable.PASSWORD.getColumnName()));
 			}
-
 			if (user == null) {
 				return LoginResults.NO_SUCH_USER;
-
 			} else {
 				if (!user.getPassword().equals(password)) {
 					return LoginResults.INVALID_PASSWORD;
@@ -51,46 +44,39 @@ public class UserServiceImpl implements UserService{
 		} catch (Exception e) {
 			throw e;
 		} finally {
-			con = null;
-			rs = null;
-			ps = null;
+			con = null; rs = null; ps = null;
 		}
-
 	}
 
 	@Override
 	public User loadUserByEmail(String email) {
-
 		User user = null;
-		String query = "Select * from users.users  where email = ? ";
+		StringBuilder query = new StringBuilder("Select * from users.users where ");
+		query.append(UsersTable.EMAIL.getColumnName() + " = ?");
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-			con = DButil.getConnection();//.getConnectionPool();
-			ps = con.prepareStatement(query);
+			con = DButil.getConnection();
+			ps = con.prepareStatement(query.toString());
 			ps.setString(1, email);
 			rs = ps.executeQuery();
 
 			while (rs.next()) {
 				user = new User();
-				user.setObjectid(rs.getInt("objectid"));
-				user.setUsername(rs.getString("username"));
-				user.setEmail(rs.getString("email"));
-				user.setPassword(rs.getString("password"));
-				user.setType(UserTypes.DEFAULT.getUserType(rs.getString("role")));
-				user.setActive(rs.getBoolean("active"));
+				user.setObjectid(rs.getInt(UsersTable.USER_ID.getColumnName()));
+				user.setUsername(rs.getString(UsersTable.USERNAME.getColumnName()));
+				user.setEmail(rs.getString(UsersTable.EMAIL.getColumnName()));
+				user.setPassword(rs.getString(UsersTable.PASSWORD.getColumnName()));
+				user.setType(UserTypes.DEFAULT.getUserType(rs.getString(UsersTable.ROLE.getColumnName())));
+				user.setActive(rs.getBoolean(UsersTable.ACTIVE.getColumnName()));
 			}
-
 			return user;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		} finally {
-			//connectionPool.close(con, ps, rs);
-			con = null;
-			rs = null;
-			ps = null;
+			con = null;	rs = null; ps = null;
 		}	  
 	}
 
@@ -105,15 +91,19 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public void addUser(User user) {
-		String query = "INSERT INTO users.users (username, email, password, role, active)"
-				+ " VALUES (?, ?, ?, ?, ?)";
+		StringBuilder query = new StringBuilder("INSERT INTO users.users(");
+		query.append(UsersTable.USERNAME.getColumnName() + ",");
+		query.append(UsersTable.EMAIL.getColumnName() + ",");
+		query.append(UsersTable.PASSWORD.getColumnName() + ",");
+		query.append(UsersTable.ROLE.getColumnName() + ",");
+		query.append(UsersTable.ACTIVE.getColumnName() + ") ");
+		query.append("VALUES(?, ?, ?, ?, ?)");
 
 		Connection con = null;
 		PreparedStatement ps = null;
-		ResultSet rs = null;
 		try {
-			con = DButil.getConnection();//.getConnectionPool();
-			ps = con.prepareStatement(query);
+			con = DButil.getConnection();
+			ps = con.prepareStatement(query.toString());
 			ps.setString(1, user.getUsername());
 			ps.setString(2, user.getEmail());
 			ps.setString(3, user.getPassword());
@@ -122,14 +112,9 @@ public class UserServiceImpl implements UserService{
 			ps.executeUpdate();
 			con.commit();
 		} catch (Exception e) {
-
 			e.printStackTrace();
-			//connectionPool.close(con, ps, rs);
 		} finally {
-			//connectionPool.close(con, ps, rs);
-			con = null;
-			rs = null;
-			ps = null;
+			con = null;	ps = null;
 		}
 	}
 
